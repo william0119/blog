3a966aca2a99576604a54b5dae422f76e9fd5d8c
William <vivi93119@163.com>
2023-04-01 22:21:12 +0800

    add config

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,50 @@
+export default {
+  title: "William's Blog",
+  description: "Keep Life Moving",
+  base: '/blog/',
+  // 没生效？？
+  // head: [["link", { rel: "icon", href: "/favicon.ico" }]],
+  // themeConfig: {
+  //   siteTitle: "William",
+  //   logo: "/logo.svg",
+  //   // 导航
+  //   nav: [
+  //     { text: "Blog", link: "/articles/js/数据类型" },
+  //     { text: "Utils", link: "/guide/utils" },
+  //     // { text: "gitee", link: "https://gitee.com/geeksdidi" },
+  //     {
+  //       text: "Interview",
+  //       items: [
+  //         {
+  //           items: [{ text: "Item A", link: "/item-1" }],
+  //         },
+  //         {
+  //           items: [
+  //             { text: "Item B", link: "/item-2" },
+  //             { text: "Item C", link: "/item-3" },
+  //           ],
+  //         },
+  //       ],
+  //     },
+  //   ],
+  //   // 社交链接
+  //   socialLinks: [{ icon: "github", link: "https://github.com/vivi0119" }],
+  //   sidebar: {
+  //     "/articles/js/": [
+  //       {
+  //         text: "JavaScript",
+  //         items: [
+  //           {
+  //             text: "js-basic",
+  //             link: "/js/数据类型",
+  //           },
+  //           {
+  //             text: "js-core",
+  //             link: "/js/数据类型",
+  //           },
+  //         ],
+  //       },
+  //     ],
+    // },
+  // },
+};
